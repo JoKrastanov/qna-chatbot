7c1e2c76234d0cfdabc111973b638ad4534da9b5
Joan <jokrastanov@gmail.com>
2023-11-05 17:02:27 +0100

    update the gpt prompt + adding chat history

--- SupportAI/qanda.py
+++ SupportAI/qanda.py
@@ -1,10 +1,19 @@
 from openai import ChatCompletion
 
-openai_model = "gpt-3.5-turbo"
+openai_model = "gpt-4"
+
+chat_history = []
 
 def create_prompt(context, query):
-    header = "Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text and requires some latest information to be updated, print 'Sorry Not Sufficient context to answer query' \n"
-    return header + context + "\n\n" + query + "\n"  
+    header = """
+    You are a support chatbot for the Exact Globe ERP system.
+    Answer the question as truthfully as possible using the provided context in a couple of sentences
+    You can use the provided chat history to get additional context regarding the question.
+    If the answer is not contained within the text and requires further information to be answered,
+    respond with 'I'm sorry, I do not have that information.'
+    """
+    chat_history.append("User:" + query)
+    return header + "Context:" + context + "\n\n" + "Question:" + query + "\n\n" + "Chat history:" + ' '.join([str(elem) for elem in chat_history]) + "\n" 
 
 def get_answer(context, query):
     """ Sends the prompt to a specified OpenAI model
@@ -27,4 +36,5 @@ def get_answer(context, query):
         ]
     )
     response_value = response['choices'][0]['message']['content']
+    chat_history.append("Chatbot:" + response_value)
     return response_value
