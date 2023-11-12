cb02d3afcf121af9d4de24aebade52cdd7a237d5
Joan <jokrastanov@gmail.com>
2023-11-12 14:07:02 +0100

    fixed setup and run script

--- setup_and_run.ps1
+++ setup_and_run.ps1
@@ -1,7 +1,6 @@
 # 1. Check Python version and install if not 3.10
-$pythonVersion = (python --version 2>&1) -match '(\d+\.\d+)' | Out-Null; $matches[1]
-if ($null -eq $pythonVersion -or $matches[1] -ne '3.10') {
-    # Download and install Python 3.10
+$pythonVersion = python --version
+if ($null -eq $pythonVersion -or $pythonVersion -notlike 'Python 3.10*') {
     Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe' -OutFile 'python_installer.exe'
     Start-Process -Wait -FilePath '.\python_installer.exe' -ArgumentList '/quiet', 'InstallAllUsers=1', 'PrependPath=1'
     Remove-Item -Path 'python_installer.exe'
@@ -9,15 +8,14 @@ if ($null -eq $pythonVersion -or $matches[1] -ne '3.10') {
 
 # 2. Check if pip is installed, if not install it
 if (-not (Get-Command pip -ErrorAction SilentlyContinue)) {
-    # Install pip
     (Invoke-WebRequest -Uri 'https://bootstrap.pypa.io/get-pip.py' -UseBasicParsing).Content | python -
 }
 
-# 3. Run Activate.ps1 script
+# 3. Activate Python venv(virtual environment)
 .\.venv\Scripts\Activate.ps1
 
-# 4. Run pip install -r ./SupportAI/requirements.txt
+# 4. Install app requirements
 pip install -r .\SupportAI\requirements.txt
 
-# 5. Run streamlit run ./SupportAI/main.py
+# 5. Run application
 streamlit run .\SupportAI\main.py
